467fcb7d3a29941e756cca57856c851537d170c8
Hugo Diogo <1883@stu.ipbeja.pt>
2026-01-07 18:59:58 +0000

    Criação do ficheiro de exercicio 4

---
+++ ex4_powm.c
@@ -0,0 +1,9 @@
+/*
+ * Exercício 4: Exponenciação Modular
+ * Requisitos:
+ * - Calcular base^exp % mod [cite: 12, 13]
+ * - Funcionar em ciclo até ordem de saída [cite: 14]
+ */
+
+#include <stdio.h>
+#include <gmp.h>
